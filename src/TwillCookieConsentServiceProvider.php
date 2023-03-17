381dd3bc62751b8047706c1b082c0a7028815335
jefsev <sevinga@protonmail.com>
2023-03-17 15:32:49 +0100

    update boot

--- src/TwillCookieConsentServiceProvider.php
+++ src/TwillCookieConsentServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace BKCmedia\TwillCookieConsent;
 
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\View;
 use A17\Twill\TwillPackageServiceProvider;
+
 use A17\Twill\Facades\TwillBlocks;
 
 class TwillCookieConsentServiceProvider extends TwillPackageServiceProvider
@@ -31,5 +34,13 @@ class TwillCookieConsentServiceProvider extends TwillPackageServiceProvider
 
         // Routes
         $this->loadRoutesFrom(__DIR__.'/../routes/cookie-consent.php');
+
+        // Register the views for the Cookie Capsule with Twill's view finder
+        Config::set('view.paths', array_merge(
+            Config::get('view.paths'),
+            [__DIR__.'/../resources/views']
+        ));
+
+        View::addNamespace('twill-cookie-consent', __DIR__.'/../resources/views/twill');
     }
 }
