e4b34de1e24f31a4f3b1817d76bd9288228dce08
JeffreySevinga <j.sevinga@bkcmedia.nl>
2023-03-20 15:50:44 +0100

    change some frontend

--- resources/views/components/cookie-consent.blade.php
+++ resources/views/components/cookie-consent.blade.php
@@ -1,11 +1,11 @@
-<div class="cookie-consent bg-black/30 fixed inset-0 w-screen h-screen">
-    <div class="fixed bottom-9 inset-x-6 mx-auto z-50 bg-white max-w-6xl shadow-2xl p-6 flex items-center justify-between flex-wrap">
-        <div class="cookie-consent__content w-full lg:w-1/2 mb-5 lg:mb-0">
+<div class="cookie-consent bg-black/30 fixed inset-0 w-screen h-screen z-30">
+    <div class="fixed bottom-9 inset-x-6 mx-auto z-50 bg-white max-w-6xl w-full shadow-2xl p-6 rounded flex items-center justify-between flex-wrap z-40">
+        <div class="cookie-consent__content w-full lg:w-3/4 mb-5 lg:mb-0">
             {!! $cookiesData->cookie_banner_description !!}
         </div>
-        <div class="cookie-consent__actions w-full lg:w-1/2 flex justify-start lg:justify-end items-center gap-3">
+        <div class="cookie-consent__actions w-full lg:w-1/4 flex justify-start lg:justify-end items-center gap-3">
             <span class="cookie-consent__trigger underline" id="opemSettings">Cookie instellingen</span>
-            <button class="cookie-consent__button bg-green text-white px-4 py-2 rounded-lg" id="acceptAllCookies">Alle cookies accepteren</button>
+            <button class="cookie-consent__button bg-green text-white px-4 py-3 rounded-lg" id="acceptAllCookies">Alle cookies accepteren</button>
         </div>
     </div>
 </div>
