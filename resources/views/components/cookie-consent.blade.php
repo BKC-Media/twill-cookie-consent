bad252acc4670e4975b12d3c9a77a5553a2dd6ca
JeffreySevinga <j.sevinga@bkcmedia.nl>
2023-03-20 15:36:22 +0100

    remove dd, i need to fix path instead of vcs someday

--- resources/views/components/cookie-consent.blade.php
+++ resources/views/components/cookie-consent.blade.php
@@ -1,5 +1,3 @@
-@dd($cookiesData)
-
 <div class="cookie-consent bg-black/30 fixed inset-0 w-screen h-screen">
     <div class="fixed bottom-9 inset-x-6 mx-auto z-50 bg-white max-w-6xl shadow-2xl p-6 flex items-center justify-between flex-wrap">
         <div class="cookie-consent__content w-full lg:w-1/2 mb-5 lg:mb-0">
