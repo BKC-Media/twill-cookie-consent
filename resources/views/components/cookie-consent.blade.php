951c9793314ff1fc8667f9bd69c545b8f012fb6a
JeffreySevinga <j.sevinga@bkcmedia.nl>
2023-03-20 15:33:12 +0100

    input()

--- resources/views/components/cookie-consent.blade.php
+++ resources/views/components/cookie-consent.blade.php
@@ -1,7 +1,7 @@
 <div class="cookie-consent bg-black/30 fixed inset-0 w-screen h-screen">
     <div class="fixed bottom-9 inset-x-6 mx-auto z-50 bg-white max-w-6xl shadow-2xl p-6 flex items-center justify-between flex-wrap">
         <div class="cookie-consent__content w-full lg:w-1/2 mb-5 lg:mb-0">
-            {!! $cookiesData->cookie_banner_description !!}
+            {!! $cookiesData->input('cookie_banner_description') !!}
         </div>
         <div class="cookie-consent__actions w-full lg:w-1/2 flex justify-start lg:justify-end items-center gap-3">
             <span class="cookie-consent__trigger underline" id="opemSettings">Cookie instellingen</span>
